0b9c3d504cfc39ce315b2d6da2ce984752e4053c
Verin <verinvld@yandex.ru>
2020-11-12 19:16:43 +0400

    81

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/util.js",
+    "./js/card.js",
+    "./js/load.js",
+    "./js/upload.js",
+    "./js/xhr.js",
+    "./js/debouncing.js",
+    "./js/pin.js",
+    "./js/map.js",
+    "./js/form.js",
+    "./js/message.js",
+    "./js/move.js",
+    "./js/filter.js"
+  ],
+  output: {
+    filename: "bundler.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+
+
+}
